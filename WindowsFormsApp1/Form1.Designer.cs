4fad9377569f097d276671c8f404a3e3d0db791d
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2022-11-03 13:18:35 +0100

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(32, 52);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2022, 11, 3, 0, 0, 0, 0);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(35, 87);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "muž";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(35, 123);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "žena";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tvorba rodného čísla";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Před 1.1.1954:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(346, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Po 1.1.1954:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Vytvořit rodné číslo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(349, 76);
+            this.maskedTextBox1.Mask = "000000\\/000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(115, 20);
+            this.maskedTextBox1.TabIndex = 10;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(349, 164);
+            this.maskedTextBox2.Mask = "000000\\/0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(115, 20);
+            this.maskedTextBox2.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(235, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 45);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Statistiky";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.maskedTextBox4);
+            this.groupBox1.Controls.Add(this.maskedTextBox3);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(538, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 232);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "v";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, -3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nejstarsi ze zadanych je:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "_____________";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(1, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(211, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Nejmladsi ze zadanych je:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(7, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "_____________";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(9, 85);
+            this.maskedTextBox3.Mask = "000000\\/000";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(115, 20);
+            this.maskedTextBox3.TabIndex = 14;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(9, 206);
+            this.maskedTextBox4.Mask = "000000\\/000";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(115, 20);
+            this.maskedTextBox4.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "A jeho rodne cislo je:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 190);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "A jeho rodne cislo je:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(301, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 45);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Konec";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(780, 250);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rodná Čísla";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<DateTime> Datumy = new List<DateTime>();
+        List<string> Rodna_Cisla = new List<string>();
+        int baby = 0;
+        int chlapy = 0;
+
+        DateTime datum = new DateTime();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                datum = dateTimePicker1.Value;
+                string Datum = datum.ToString();
+                if (Datum.Contains("  "))
+                {
+                    Datum.Replace("  ", " ");
+                }
+                string[] Datum_Rozdelene = Datum.Split(' ');
+                string[] Datum_vice_rozdelene = Datum_Rozdelene[0].Split('.');
+                if (radioButton1.Checked)
+                {
+                    chlapy += 1;
+                }
+                else if (radioButton2.Checked)
+                {
+                    int X = int.Parse(Datum_vice_rozdelene[1]);
+                    X += 50;
+                    Datum_vice_rozdelene[1] = X.ToString();
+                    baby += 1;
+                }
+                string str = Datum_vice_rozdelene[2];
+                string vysledek = str.Substring(str.Length - 2);
+                string Rodne_cislo = " ";
+                Rodne_cislo = Datum_vice_rozdelene[0] + Datum_vice_rozdelene[1] + vysledek;
+
+                bool delitelne3 = false;
+                bool Delitelne3 = false;
+
+                string Rodne_cislo_delitelne_tremi_Stare = " ";
+                string Rodne_cislo_delitelne_tremi_Nove = " ";
+
+                Random rng = new Random();
+                while (delitelne3 == false)
+                {
+                    int num = rng.Next(110, 990);
+                    Rodne_cislo_delitelne_tremi_Stare = Rodne_cislo + num;
+                    if ((Int64.Parse(Rodne_cislo_delitelne_tremi_Stare) % 3) == 0)
+                    {
+                        delitelne3 = true;
+                    }
+                }
+
+                while (Delitelne3 == false)
+                {
+                    int num = rng.Next(1100, 9900);
+                    Rodne_cislo_delitelne_tremi_Nove = Rodne_cislo + num;
+                    if ((Int64.Parse(Rodne_cislo_delitelne_tremi_Nove) % 3) == 0)
+                    {
+                        Delitelne3 = true;
+                    }
+                }
+
+                maskedTextBox1.Text = Rodne_cislo_delitelne_tremi_Stare;
+                maskedTextBox2.Text = Rodne_cislo_delitelne_tremi_Nove;
+
+                Rodna_Cisla.Add(Rodne_cislo_delitelne_tremi_Nove);
+                Datumy.Add(datum);
+            } catch
+            {
+                MessageBox.Show("neco se pokazilo, jedna z verzí je že jste nezadal žádné datum");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime Nejstarsi = Datumy[0];
+                DateTime Nejmladsi = Datumy[0];
+                foreach (DateTime x in Datumy)
+                {
+                    if (x > Nejmladsi)
+                    {
+                        Nejmladsi = x;
+                    }
+                    else if (x < Nejstarsi)
+                    {
+                        Nejstarsi = x;
+                    }
+                }
+
+                string[] Nejstarsi_lepsi_vize = Nejstarsi.ToString().Split(' ');
+                label5.Text = "Datum narozeni: " + Nejstarsi_lepsi_vize[0];
+                int stari = Datumy.IndexOf(Nejstarsi);
+                maskedTextBox3.Text = Rodna_Cisla[stari];
+
+                string[] Nejmladsi_lepsi_vize = Nejmladsi.ToString().Split(' ');
+                label8.Text = "Datum narozeni: " + Nejmladsi_lepsi_vize[0];
+                int mlady = Datumy.IndexOf(Nejmladsi);
+                maskedTextBox4.Text = Rodna_Cisla[mlady];
+            }
+            catch
+            {
+                MessageBox.Show("neco se pokazilo, jedna z verzí je že jste nezadal žádné datum");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
